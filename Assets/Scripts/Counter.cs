47b040d1bc6e5fc1d789a2c1f5a897bc5a0f9d34
adrimarco <adrianmarco-@hotmail.es>
2024-02-16 13:06:52 +0100

    feat: add ui

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    private float               currentTime;
+    [SerializeField]
+    private PlayerController    playerController;
+    [SerializeField]
+    private TMP_Text            scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerController != null && !playerController.gameOver) {
+            currentTime += Time.deltaTime;
+
+            UpdateTimeDisplay();
+        }
+    }
+
+    private void UpdateTimeDisplay() {
+        if (scoreText == null) return;
+
+        scoreText.text = "Time: " + currentTime.ToString("F1");
+    }
+}
